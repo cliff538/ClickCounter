7e4685ba96aefe0dcc3ae4ad15ee6be26c3c9292
cliff538 <cliff538@yahoo.com>
2015-03-25 08:43:51 -0600

    Initial Setup

---
+++ Click Counter/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Cliff Gurske on 3/25/15.
+//  Copyright (c) 2015 cat3Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		var label = UILabel()
+		label.frame = CGRectMake(150, 150, 60, 60)
+		label.text = "0"
+		
+	}
+
+}
+
